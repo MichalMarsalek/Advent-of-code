4d8bc4059ed85570353c8521ff3ab3482893c9c3
MichalMarsalek <Michal.Marsalek4@gmail.com>
2015-12-17 19:59:57 +0100

    New days

--- Day 16.py
+++ Day 16.py
@@ -1,7 +1,21 @@
 def solve(input):
-    
+    mySue = {"children": 3, "cats": 7, "samoyeds": 2, "pomeranians": 3, "akitas": 0, "vizslas": 0, "goldfish": 5, "trees": 3, "cars": 2, "perfumes": 1}
+    Sues = {name: {prop: int(value) for prop, value in [x.split(": ") for x in props.split(", ")]} for name, props in [y.split(": ", 1) for y in input.split("\n")]}
+    for Sue in Sues:
+        if all(compare(mySue, Sues[Sue], prop) for prop in Sues[Sue]):
+            part1 = Sue
+        if all(compare(mySue, Sues[Sue], prop, True) for prop in Sues[Sue]):
+            part2 = Sue
     return part1, part2
-    
+
+def compare(mySue, Sue, prop, betterCompare = False):
+    if betterCompare:
+        if prop in ("cats", "trees"):
+            return mySue[prop] < Sue[prop]
+        elif prop in ("pomeranians", "goldfish"):
+            return mySue[prop] > Sue[prop]
+    return mySue[prop] == Sue[prop]
+
 def run(day):
     input = input = open('Day ' + str(day) + ' input.txt').read()
     print(solve(input))
--- Day 17.py
+++ Day 17.py
@@ -1,9 +1,35 @@
+IT = 0
 def solve(input):
-    
+    containers = list(map(int, input.split("\n")))
+    posses = find(containers, 150)
+    minlen = min(map(len, posses))
+    posses2 = [x for x in posses if len(x) == minlen]
+    part1 = len(posses)
+    part2 = len(posses2)
     return part1, part2
-    
+
+def find(containers, volume):
+    global IT
+    IT += 1
+    fitting = [cont for i, cont in enumerate(containers) if cont <= volume and sum(containers[i:]) >= volume]
+    if len(fitting) == 0:
+        if volume == 0:
+            return [[]]
+        return None
+    suma = []
+    for i, cont in enumerate(containers):
+        if cont not in fitting:
+            continue
+        sols = find(containers[i+1:], volume - cont)
+        if sols is None:
+            continue
+        for sol in sols:
+            suma.append([cont] + sol)
+    return suma
+
 def run(day):
     input = input = open('Day ' + str(day) + ' input.txt').read()
     print(solve(input))
 
 run(17)
+print(IT)
