c8b22ac8a14537f488304814ee7e58116ffda159
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2020-12-18 07:14:39 +0100

    Day 18

---
+++ 2020/Python/day18.py
@@ -0,0 +1,30 @@
+from aoc import *
+
+class Int:
+    def __init__(self, v):
+        self.v = v
+    
+    def __sub__(a,b):
+        return Int(a.v*b.v)
+    def __add__(a,b):
+        return Int(a.v+b.v)
+    def __mul__(a,b):
+        return Int(a.v+b.v)
+    def __int__(self):
+        return self.v
+
+def transform(inp, part):
+    for c in "0123456789":
+        inp = inp.replace(c, f"Int({c})")
+    inp = inp.replace("*", "-")
+    inp = inp.replace("+", "+*"[part])
+    return inp
+
+def solve(inp):
+    data = inp.splitlines()
+    part1 = sum(int(eval(transform(x, 0))) for x in data)
+    part2 = sum(int(eval(transform(x, 1))) for x in data)
+    return part1, part2
+
+
+run(solve)
