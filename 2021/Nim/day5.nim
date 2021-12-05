e5d6b35e95c3fcb4766118476ac64292ac48bd06
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2021-12-05 08:25:36 +0100

    Update day5.nim

--- 2021/Nim/day5.nim
+++ 2021/Nim/day5.nim
@@ -4,12 +4,11 @@ day 5:
     var p1, p2: CountTable[(int,int)]
     for line in lines:
         var (ok, x1, y1, x2, y2) = line.scanTuple("$i,$i -> $i,$i")
-        if x1 == x2 or y1 == y2 or (x2-x1) == (y2-y1) or (x2-x1) == -(y2-y1):
-            for i in 0..max(max(x1, x2)-min(x1, x2),max(y1, y2)-min(y1, y2)):
-                let X = x1 + i * sgn(x2-x1)
-                let Y = y1 + i * sgn(y2-y1)
-                if x1 == x2 or y1 == y2:
-                    p1.inc (X,Y)
-                p2.inc (X,Y)
+        for i in 0..max(max(x1, x2)-min(x1, x2),max(y1, y2)-min(y1, y2)):
+            let X = x1 + i * sgn(x2-x1)
+            let Y = y1 + i * sgn(y2-y1)
+            if x1 == x2 or y1 == y2:
+                p1.inc (X,Y)
+            p2.inc (X,Y)
     part 1: p1.keys.countIt (p1[it] > 1)
     part 2: p2.keys.countIt (p2[it] > 1)
