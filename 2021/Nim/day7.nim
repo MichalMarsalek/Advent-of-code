65f3c2196c507d517b2baa736c20c08a4552a8e0
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2021-12-07 11:24:03 +0100

    day 7 is now provably correct

--- 2021/Nim/day7.nim
+++ 2021/Nim/day7.nim
@@ -1,8 +1,13 @@
 include aoc
-import stats
 
 day 7:
-    var s = sorted ints
+    var
+     s = sorted ints
+     median = s[500]
+     mean   = s.sum div s.len
     func price(x=0):int = x*(x+1) div 2
-    part 1: s.mapIt(abs(it-s[500])).sum # stats doesn't have median???
-    part 2: s.mapIt(price(abs(it-s.mean.int))).sum
+    part 1: s.mapIt(abs(it-median)).sum
+    part 2: min(
+                s.mapIt(price(abs(it-mean))).sum,
+                s.mapIt(price(abs(it-mean-1))).sum
+            )
