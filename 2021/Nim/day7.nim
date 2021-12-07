ee933d40037145e38bb0d7ff0f60a2b37fe4e5e8
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2021-12-07 06:22:44 +0100

    Update day7.nim

--- 2021/Nim/day7.nim
+++ 2021/Nim/day7.nim
@@ -3,8 +3,6 @@ import stats
 
 day 7:
     var s = sorted ints
-    proc price(x=0):int = x*(x+1) div 2
-    part 1:
-        ints.mapIt(abs(it-s[499])).sum # stats doesn't have median???
-    part 2:
-        ints.mapIt(price(abs(it-s.mean.int))).sum
+    func price(x=0):int = x*(x+1) div 2
+    part 1: s.mapIt(abs(it-s[500])).sum # stats doesn't have median???
+    part 2: s.mapIt(price(abs(it-s.mean.int))).sum
