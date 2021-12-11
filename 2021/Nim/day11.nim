ad21e6183ae596a039dd5a5407a875451c566aba
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2021-12-11 08:50:48 +0100

    Update day11.nim

--- 2021/Nim/day11.nim
+++ 2021/Nim/day11.nim
@@ -3,23 +3,20 @@ include aoc
 day 11:    
     proc update(grid: var Grid[int]):int =
         var q = grid.coordinates.toSeq
-        var flashed: HashSet[Point]
+        for p in q: grid[p] += 1
         while q.len > 0:
             var p = q.pop
-            grid[p] += 1
-            if grid[p] >= 10 and p notin flashed:
-                flashed.incl p
-                for P in grid.neighbours(p,directions8):
-                    q.add P
-        for p in grid.coordinates:
             if grid[p] >= 10:
                 grid[p] = 0
-        return flashed.len        
+                inc result
+                for P in grid.neighbours(p,directions8):
+                    if grid[P] > 0:
+                        grid[P] += 1
+                        q.add P
     
     var data = lines.map(l => l.mapIt(parseInt $it))
-    part 1,int:
-        for i in 1..100:
-            result += update(data)
+    part 1:
+        sum mapIt(1..100, update(data))
     part 2:
         for i in 101..10000:
             if update(data) == data.size:
