e8d4ee9d1a004f1c29b75bb9bb66e5c558a7138a
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2021-12-13 21:10:46 +0100

    day 5 cleanup

---
+++ 2021/Nim/day14.nim
@@ -0,0 +1,8 @@
+include aoc
+
+day 14:    
+    
+    part 1:
+        0
+    part 2:
+        0
--- 2021/Nim/day5.nim
+++ 2021/Nim/day5.nim
@@ -1,14 +1,13 @@
 include aoc
 
 day 5:
-    var p1, p2: CountTable[(int,int)]
+    var p1, p2: CountTable[Point]
     for line in lines:
         var (ok, x1, y1, x2, y2) = line.scanTuple("$i,$i -> $i,$i")
         for i in 0..max(abs(x1-x2),abs(y1-y2)):
-            let X = x1 + i * sgn(x2-x1)
-            let Y = y1 + i * sgn(y2-y1)
+            let P = (x1,x2) + i * (sgn(x2-x1), sgn(y2-y1))
             if x1 == x2 or y1 == y2:
-                p1.inc (X,Y)
-            p2.inc (X,Y)
+                p1.inc P
+            p2.inc P
     part 1: p1.keys.countIt (p1[it] > 1)
     part 2: p2.keys.countIt (p2[it] > 1)
