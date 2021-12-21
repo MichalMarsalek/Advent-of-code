81b430e1368b7558140adf421e3e710f826ddc96
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2021-12-21 07:59:56 +0100

    Update day21.nim

--- 2021/Nim/day21.nim
+++ 2021/Nim/day21.nim
@@ -18,14 +18,14 @@ day 21:
                 return scores[1-turn] * countRolls
             turn = 1 - turn
     part 2:
-        let rolls = product([@[1,2,3],@[1,2,3],@[1,2,3]]).mapIt it.sum
+        const rolls = {3: 1, 4: 3, 5: 6, 6: 7, 7: 6, 8: 3, 9: 1}
         func countWins(p1, p2, s1, s2: int): array[2, int] {.memoized.} =
             if s1 >= 21 or s2 >= 21:
                 return [int(s1>=21), int(s2>=21)]
-            for roll in rolls:
+            for (roll,freq) in rolls:
                 let p = (p1 + roll - 1) mod 10 + 1
                 let w = countWins(p2, p, s2, s1 + p)
-                result[0] += w[1]
-                result[1] += w[0]
+                result[0] += freq*w[1]
+                result[1] += freq*w[0]
         
         max countWins(ints[1], ints[3], 0, 0)
