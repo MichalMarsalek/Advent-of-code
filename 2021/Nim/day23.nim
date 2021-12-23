19237be54f1d37c892d99073f7c5d26d2b663c1f
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2021-12-23 09:49:06 +0100

    day 23 both parts

--- 2021/Nim/day23.nim
+++ 2021/Nim/day23.nim
@@ -1,38 +1,50 @@
 include aoc
 import heapqueue
 
-type State = array[8,Point]
+type State = array[4,array[4,Point]]
 func `<`(a,b:(int,State)):bool =
     a[0] < b[0]
 
 day 23:
-    echo input
-    var state: State
-    var seenType = [0,0,0,0]
-    for y,row in lines:
-        for x,c in row:
-            if 'A' <= c and c <= 'D':
-                let o = c.ord - 'A'.ord
-                state[o*2+seenType[o]] = (x,y)
-                seenType[o] += 1
-    var initState = state
-    echo state
+    proc normalize(a: var State) =
+        for i in 0..3:
+            sort a[i]
     
-    #func getState1(input:string) =
-        
+    func cost(i:int):int = 10^i
+    func goalX(i:int):int = (3 + i * 2)
     
-    func cost(i:int):int = 10^(i div 2)
-    func goalX(i:int):int = (3 + i div 2 * 2)
+    func contains(v:State, p:Point): bool =
+        for group in v:
+            for emp in group:
+                if emp == p: return true
     
-    
-    func isWin(state:State,i:int):bool =
-        state[(i div 2 * 2)..(i div 2 * 2 + 1)].toHashSet == [(i.goalX, 2),(i.goalX, 3)].toHashSet
+    func getState(input:string, part:range[1..2]): State =
+        var lines = input.splitLines
+        if part == 1:
+            lines = lines[0..3] & "  #A#B#C#D#" & "  #A#B#C#D#"
+        else:
+            lines = lines[0..2] & "  #D#C#B#A#" & "  #D#B#A#C#" & lines[3]
+        var seenType = [0,0,0,0]
+        for y,row in lines:            
+            for x,c in row:
+                if 'A' <= c and c <= 'D':
+                    let o = c.ord - 'A'.ord
+                    result[o][seenType[o]] = (x,y)
+                    seenType[o] += 1
+        normalize result
     
     func isWin(state:State):bool =
         for i in 0..3:
-            if state[2*i..2*i+1].toHashSet != [((2*i).goalX, 2),((2*i).goalX, 3)].toHashSet:
+            if state[i] != {i.goalX:2, i.goalX:3, i.goalX:4, i.goalX:5}:
                 return false
         return true
+    
+    func countCorrect(state:State,i:int):int =
+        card({i.goalX:2, i.goalX:3, i.goalX:4, i.goalX:5}.toHashSet * state[i].toHashSet)
+    func countAll(state:State,x:int):int =
+        for group in state:
+            for amp in group:
+                if amp.x == x: inc result
     
     iterator trackMovement(a,b:int):int =
         var a = a
@@ -41,87 +53,77 @@ day 23:
             a += sgn(b-a)
         yield a
     
-    iterator moveOut(v:State, i:int): (int,State) =
+    iterator moveOut(v:State, i,j:int): (int,State) =
         const stops = [1,2,4,6,8,10,11]
-        var s = v[i].x
+        var s = v[i][j].x
         while (s-1, 1) notin v:
             s -= 1
             if s < 1: break
             if s notin stops: continue
             var copy = v
-            copy[i] = (s,1)
-            yield ((v[i].x-s + 1)*i.cost, copy)
-        s = v[i].x
+            copy[i][j] = (s,1)
+            normalize copy
+            yield ((v[i][j].x-s + v[i][j].y - 1)*i.cost, copy)
+        s = v[i][j].x
         while (s+1, 1) notin v:
             s += 1
             if s > 11: break
             if s notin stops: continue
             var copy = v
-            copy[i] = (s,1)
-            yield ((s-v[i].x + 1)*i.cost, copy)
+            copy[i][j] = (s,1)
+            normalize copy
+            yield ((s-v[i][j].x + v[i][j].y - 1)*i.cost, copy)
     
     proc debugState(state:State) =
-        var t = input
-        for c, p in initState:
-            t[p.x + p.y * 14] = '.'
-        for c, p in state:
-            t[p.x + p.y * 14] = (c div 2 + 'A'.ord).chr
+        var t = """
+#############
+#...........#
+###.#.#.#.###
+  #.#.#.#.#  
+  #.#.#.#.#  
+  #.#.#.#.#  
+  #########"""
+        for c, g in state:
+            for p in g:
+                t[p.x + p.y * 14] = (c + 'A'.ord).chr
         echo t
         echo ""
     
-    
-    
-    iterator moveIn(v:State, i:int): (int, State) =
+    iterator moveIn(v:State, i,j:int): (int, State) =
         block all:
-            if v[i div 2 * 2]     != (i.goalX, 3) and
-               v[i div 2 * 2 + 1] != (i.goalX, 3) and
-               (3 + i div 2 * 2, 3) in v:
-                break all
-            for x in trackMovement(v[i].x, i.goalX):
-                if (x, 1) in v and v[i] != (x,1):
-                    break all
-            if (i.goalX, 3) in v:
+            if v.countCorrect(i) == v.countAll(i.goalX):
+                for x in trackMovement(v[i][j].x + sgn(i.goalX-v[i][j].x), i.goalX):
+                    if (x, 1) in v:
+                        break all
                 var copy = v
-                copy[i] = (i.goalX, 2)
-                yield (i.cost * (abs(i.goalX - v[i].x) + 1), copy)
-            else:
-                var copy = v
-                copy[i] = (i.goalX, 3)
-                yield (i.cost * (abs(i.goalX - v[i].x) + 2), copy)
-        
+                copy[i][j] = (i.goalX, 5-v.countCorrect(i))
+                normalize copy
+                yield ((abs(v[i][j].x - i.goalX) + 4 - v.countCorrect(i)) * i.cost, copy)        
     
     
     iterator neighbours(v:State): (int,State)=
         var needToMoveOut = true
-        for i in 0..7:
-            if v[i].y == 1:
-                for x in moveIn(v, i):
-                    yield x
-                    needToMoveOut = false
+        for i in 0..3:
+            for j in 0..3:
+                if v[i][j].y == 1:
+                    for x in moveIn(v, i, j):
+                        yield x
+                        needToMoveOut = false
         if needToMoveOut:
-            for i in 0..7:
-                if v[i].x in [3,5,7,9]:                
-                    if v[i].y == 2 and not v.isWin(i):
-                        for x in moveOut(v, i):
-                            yield x
-                    if v[i].x != i.goalX and v[i].y == 3 and v[i]-(0,1) notin v:
-                        for (p,x) in moveOut(v, i):
-                            yield (p+i.cost,x)
+            for i in 0..3:
+                for j in 0..3:
+                    if v[i][j].x in [3,5,7,9]:
+                        if anyIt(2..<v[i][j].y, (v[i][j].x, it) in v): continue
+                        if v[i][j].x != i.goalX or v.countCorrect(i) != v.countAll(i.goalX):
+                            for x in moveOut(v, i, j):
+                                yield x
     
-    #for (p,x) in neighbours(state):
-    #    echo p
-    #    debugState(x)
-    #echo "n done"
-    
-    part 1:
-        var dist = {state: 0}.toTable
-        var seen = [state].toHashSet
-        var q = {0:state}.toHeapQueue
+    proc solve(initialState:State):int =
+        var dist = {initialState: 0}.toTable
+        var seen = [initialState].toHashSet
+        var q = {0:initialState}.toHeapQueue
         while true:
-            let vv = q.pop
-            let v = vv[1]
-            #echo vv[0]
-            #debugState v
+            let v = q.pop[1]
             if v.isWin: return dist[v]
             seen.incl v
             for price,u in neighbours(v):
@@ -130,5 +132,5 @@ day 23:
                     if newCost < dist.getOrDefault(u, 2^60):                
                         dist[u] = newCost
                         q.push (dist[u], u)
-    part 2:
-        0
+    part 1: solve input.getState 1
+    part 2: solve input.getState 2
