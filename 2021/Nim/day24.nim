7b9b16cab93d3fe7dd14025ae6bdd8bd9b41857f
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2021-12-24 09:23:56 +0100

    Update day24.nim

--- 2021/Nim/day24.nim
+++ 2021/Nim/day24.nim
@@ -2,8 +2,8 @@ include aoc
 
 day 24:
     var ins: Grid[int]
-    for blck in lines.distribute(14):
-        var L = blck.mapIt(it.split)
+    for blck in lines.distribute 14:
+        var L = blck.mapIt it.split
         ins.add [L[4][2], L[5][2], L[15][2]].map parseInt
     
     var ranges: array[14, Slice[int]]
@@ -12,9 +12,9 @@ day 24:
         if A[0] == 1:
             stack.add i
         else:
-            var j = stack.pop
-            var B = ins[j]
-            var diff = B[2]+A[1]
+            let j = stack.pop
+            let B = ins[j]
+            let diff = B[2]+A[1]
             if diff < 0:
                 ranges[j] = 1-diff..9
                 ranges[i] = 1..9+diff
