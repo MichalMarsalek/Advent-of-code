eefb9a627b050f4412ad8241b915181a010075d0
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-12-01 06:05:09 +0100

    solve day 1

--- 2022/Nim/day1.nim
+++ 2022/Nim/day1.nim
@@ -1,7 +1,6 @@
 include aoc
 
 day 1:
-    part 1:
-        0
-    part 2:
-        58
+    let calories = input.split("\n\n").map(x => x.ints.sum)
+    part 1: calories.max
+    part 2: calories.sorted[^3..^1].sum
