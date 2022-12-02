23715224f0f633bd617638a0a4f0d5f7e0e2e918
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-12-02 06:46:39 +0100

    solve day 2

---
+++ 2022/Nim/day2.nim
@@ -0,0 +1,15 @@
+include aoc
+
+day 2:
+    let rounds = lines.mapIt (
+            it[0].ord - 'A'.ord,
+            it[2].ord - 'X'.ord
+        )
+    func score(a, b: int): int =
+        ((b - a + 4) mod 3) * 3 + b + 1
+    part 1, int:
+        for (a, b) in rounds:
+            result += score(a, b)
+    part 2, int:
+        for (a, b) in rounds:
+            result += score(a, (a + b + 2) mod 3)
