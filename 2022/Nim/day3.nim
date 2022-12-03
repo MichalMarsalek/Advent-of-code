60601fa041d7aec50c7e2414de4e44c37921c97f
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-12-03 08:47:42 +0100

    simplify day 3

--- 2022/Nim/day3.nim
+++ 2022/Nim/day3.nim
@@ -5,6 +5,5 @@ day 3:
         x.ord - (if x >= 'a': 'a'.ord - 1 else: 'A'.ord - 27)
     func common(parts: seq[string]): char =
         parts.mapIt(it.toHashSet).foldl(a*b).toSeq[0]
-    let priorityOfCommon = priority * common
-    part 1: sum lines.mapIt(it.distribute 2).map priorityOfCommon
-    part 2: sum lines.groupsOf(3).map priorityOfCommon
+    part 1: sum lines.mapIt(it.distribute 2).map priority * common
+    part 2: sum lines.groupsOf(3).map priority * common
