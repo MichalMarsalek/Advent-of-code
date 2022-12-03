81e04f2c9df8df587458f7ff420cdf8545e6bce5
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-12-03 07:58:07 +0100

    refactor day 3

--- 2022/Nim/day3.nim
+++ 2022/Nim/day3.nim
@@ -3,12 +3,12 @@ include aoc
 day 3:
     func priority(x: char): int =
         x.ord - (if x >= 'a': 'a'.ord - 1 else: 'A'.ord - 27)
-    func priorityMaxCommon(parts: varargs[string]): int =
-        max parts.mapIt(it.toHashSet).foldl(a*b).toSeq.map(priority)
+    func common(parts: varargs[string]): char =
+        parts.mapIt(it.toHashSet).foldl(a*b).toSeq[0]
     part 1, int:
         for line in lines:
             let n = line.len div 2
-            result += priorityMaxCommon(line[0..<n], line[n..^1])
+            result += priority common(line[0..<n], line[n..^1])
     part 2, int:
         for i in 0..<(lines.len div 3):
-            result += priorityMaxCommon(lines[3*i], lines[3*i+1], lines[3*i+2])
+            result += priority common(lines[3*i], lines[3*i+1], lines[3*i+2])
