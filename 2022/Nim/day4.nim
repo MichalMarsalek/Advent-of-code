2f073b14de664c36d4b862635ec6fe764046a220
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-12-04 06:47:11 +0100

    solve day 4

---
+++ 2022/Nim/day4.nim
@@ -0,0 +1,10 @@
+include aoc
+
+day 4:
+    let ranges = lines.mapIt(it.replace("-", "..").ints4)
+    part 1:
+        sum collect(
+            for (a, b, c, d) in ranges:
+                int((c >= a and d <= b) or (a >= c and b <= d))
+        )
+    part 2: sum collect(for (a, b, c, d) in ranges: int((c <= b) and (a <= d)))
