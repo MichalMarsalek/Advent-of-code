28768e9b33862f9cd2f57c4a48b3259d2a7c23dd
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-12-06 17:41:22 +0100

    use inc

--- 2022/Nim/day6.nim
+++ 2022/Nim/day6.nim
@@ -3,7 +3,7 @@ include aoc
 day 6:
     func findMarker(data: string, length: Natural): Natural =
         while data[result..<result+length].deduplicate.len < length:
-            result += 1
+            inc result
         result + length
     part 1: findMarker(input, 4)
     part 2: findMarker(input, 14)
