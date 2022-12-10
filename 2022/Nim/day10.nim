5b364bfadd19c6d3d7e70f7e9de06c4a4c5d992e
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-12-10 06:47:12 +0100

    solve day 10

---
+++ 2022/Nim/day10.nim
@@ -0,0 +1,16 @@
+include aoc
+
+day 10:
+    var xHistory = @[1]
+    for line in lines:
+        xHistory &= xHistory[^1]
+        if line != "noop":
+            xHistory &= xHistory[^1] + ints1 line
+    part 1: sum(for c in countup(20, 220, 40): c * xHistory[c-1])
+    part 2:
+        var crt = collect:
+            for y in 0..5:
+                for x in 0..39:
+                    if abs(xHistory[x+40*y] - x) <= 1:
+                        (x, y)
+        plot crt
