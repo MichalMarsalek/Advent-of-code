504543a3d2df6c2185ee17f88e7f4e1a619ef9a9
MichalMarsalek <MichalMarsalek@users.noreply.github.com>
2022-12-13 19:56:19 +0100

    clean up day 12

--- 2022/Nim/day12.nim
+++ 2022/Nim/day12.nim
@@ -6,10 +6,11 @@ day 12:
         elif x == 'E': ord('z')
         else: ord(x)
     let terrain = lines.mapIt(toSeq it)
-    let start = terrain.coordinates.toSeq.filterIt(terrain[it] == 'S')[0]
-    let finish = terrain.coordinates.toSeq.filterIt(terrain[it] == 'E')[0]
+    let coords = terrain.coordinates.toSeq
+    let start = coords.filterIt(terrain[it] == 'S')[0]
+    let finish = coords.filterIt(terrain[it] == 'E')[0]
     let distancesToFinish = terrain.neighboursRec(finish, (a, b) => elevation(a)-elevation(b) <= 1)
     part 1:
         distancesToFinish[start]
     part 2:
-        terrain.coordinates.toSeq.filterIt(terrain[it] == 'a').mapIt(distancesToFinish.getOrDefault(it, int.high)).min
+        coords.filterIt(terrain[it] == 'a').mapIt(distancesToFinish.getOrDefault(it, int.high)).min
